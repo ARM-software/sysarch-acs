455681a7c4c408616c839be540ec8a36493de04b
Balaji Gontumukkala <balaji.gontumukkala@arm.com>
2025-12-17 09:15:05 +0000

    feat(pfdi): Add test for invalid PFDI features parameters
    - Introduced pfdi022 to validate behavior with invalid input to PFDI feature
    - Updated Pfdi.inf and val_interface.h to include new test case
    - Ensures proper failure handling when incorrect x2-x4 values are passed
    
    Signed-off-by: Balaji Gontumukkala <balaji.gontumukkala@arm.com>
    Change-Id: I5f2f313d29f2febdf4086c7b436a4c194d51376e

--- val/include/val_interface.h
+++ val/include/val_interface.h
@@ -842,5 +842,6 @@ uint32_t pfdi015_entry(uint32_t num_pe);
 uint32_t pfdi016_entry(uint32_t num_pe);
 uint32_t pfdi017_entry(uint32_t num_pe);
 uint32_t pfdi021_entry(uint32_t num_pe);
+uint32_t pfdi022_entry(uint32_t num_pe);
 
 #endif
